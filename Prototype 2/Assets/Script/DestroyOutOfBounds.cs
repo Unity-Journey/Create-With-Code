3463c434b688bed75c03d5e49420bebde3adc066
Shiv Kumar Ganesh <GShiv.sk@gmail.com>
2020-05-10 20:51:26 -0700

    Completed the Prototype 2
    Completed the prototype 2

---
+++ Prototype 2/Assets/Script/DestroyOutOfBounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 0.0f;
+    public float bottomBound = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        if(transform.position.z < bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 30.0f;
+    public float horizontalMovement;
+    public GameObject foodPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalMovement * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
+        }
+    }
+}
